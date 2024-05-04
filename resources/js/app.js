f63a94f3d40dff6c1a9b34e5d0528f5fe1ba9338
Ivan Molina <ivanmolinamas@gmail.com>
2024-05-04 12:29:11 +0200

    Se intenta añadir sweetAlert2 sin exito, se da vida a las alertas

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 import './bootstrap';
+//importamos sweetAlert2
+const Swal = require('sweetalert2');
 
 import Alpine from 'alpinejs';
+
 
 window.Alpine = Alpine;
 
--- resources/views/alumnos/index.blade.php
+++ resources/views/alumnos/index.blade.php
@@ -3,9 +3,9 @@
         <h1 class="text-3xl text-black/50"> Listado de alumnos</h1>
         <!-- variable temporal de aviso de usuario creado -->
         @if (session()->get("status"))
-            <div role="alert" class="alert alert-success w-4/12 ">
+            <div id="alerta" role="alert" class="alert alert-success w-7/12 flex items-center space-x-2 ">
                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none"
-                     viewBox="0 0 24 24">
+                     viewBox="0 0 24 24" style="display: inline-block;"> <!-- se añadie innline para ver todo en una linea -->
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                           d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
                 </svg>
@@ -14,7 +14,7 @@
         @endif
 
 
-        <a href="{{route("alumnos.create")}}" class="btn btn-xs btn-primary m-2">Nuevo alumno</a>
+        <a href="{{route("alumnos.create")}}" class="btn btn-xs btn-primary m-2">Añadir nuevo alumno</a>
         <div class="w-full flex justify-center">
             <table class="table table-xs table-pin-rows table-pin-cols w-8/12">
                 <thead>
@@ -58,15 +58,58 @@
                 </tbody>
             </table>
         </div>
-        <div class="flex-col">
+        <div class="mt-3">
             {{$alumnos->links()}}
         </div>
 
     </div>
     <script>
+        // funcion con una alert simple para verificar el borrado
+
         function confirmSubmit() {
             var confirmacion = confirm("¿Quieres borrar este alumno? \nEsta operacion es irreversible")
             return confirmacion;
         }
+
+        // funcion que los alerts tengan una vida de 5 segundos
+
+        // la alerta se muestra al cargar la pagina
+        document.addEventListener("DOMContentLoaded", mostrarAlerta())
+        //generamos la funcion
+        function mostrarAlerta() {
+            var alerta = document.getElementById("alerta");
+            alerta.style.display = "block"; //mostramos el div
+            //despues de 5 segundos ocultamos el div
+            setTimeout(function () {
+                alerta.style.display = "none"; //ocultamos el div
+            }, 5000);
+        }
+
+
+        /*
+        //funcion usando sweetAlert2
+        Por alguna razón no funciona
+
+        function confirmSubmit() {
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                }
+            });
+        }
+        */
+
     </script>
 </x-layouts.layout>
